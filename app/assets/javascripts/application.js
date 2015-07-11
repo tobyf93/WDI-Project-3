da6c30601b71e4d4abbf7f1fe1c784dd8c03a9c9
Toby Flemming <tobyf93@hotmail.com>
2015-07-11 18:12:25 +1000

    Add bootstrap.  Use flex-box to divide AppView into 3 sections - categories, items, order.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,6 +12,7 @@
 //
 //= require jquery
 //= require jquery_ujs
+//= require bootstrap.min.js
 //= require underscore-min.js
 //= require backbone-min.js
 //= require_tree ./backbone/models
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
   root :to => 'pages#app'
+
+  resources :items
+
+  resources :orders do
+    resources :items
+  end
+
 end
